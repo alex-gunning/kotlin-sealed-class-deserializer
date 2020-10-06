4eae2f405c4846d66ee37121f1d3518985ac82f2
Alex Gunning <50325707+alex-gunning-jumo@users.noreply.github.com>
2020-10-06 14:20:27 +0200

    Feature detect constructor recursively (#2)
    * Almost correctly determins constructor in a recursive fashion.
    
    * Correctly determines any object-only sealed class
    
    * Remove /gradle
    
    * Remove /.gradle
    
    * Nothing

--- src/main/kotlin/org/ktdeserializer/Deserializer.kt
+++ src/main/kotlin/org/ktdeserializer/Deserializer.kt
@@ -63,112 +63,173 @@ class Deserializer<T : Any>(private val type: KClass<T>) : JsonDeserializer<T>()
             constructorList: List<KFunction<T>>,
             suppliedParamList: List<Pair<String, Any>>): KFunction<T> {
         val constructorParamList = constructorList
-                .map { getParameterListWithType(it) }
                 .map { constructorArgTree(it) }
 
-        val isASubtree = constructorParamList.map { includesSubtree(suppliedParamList, it) }
+        /*  A boolean determining whether the supplied parameters are the a subtree
+         *  of each constructor.
+         */
+        val constructorFitsParameters = constructorParamList.map { includesSubtree(suppliedParamList, it) }
 
-        /*  Any constructors which the supplied params COULD fit in to
+        /*  Constructors which the supplied params COULD fit in to
          *  (but are not necessarily valid)
          */
         val fittingConstructors = constructorParamList
-                .zip(isASubtree)
+                .zip(constructorList)
+                .zip(constructorFitsParameters)
                 .filter { it.second }
                 .map { it.first }
 
-        val tempNameForConstructorParameterName = constructorParamList
-                .map { p -> p.map { it.first } } // Just a lens for the parameter name (ignores type)
-
-        /*  Remove supplied params from each constructor in the list if the name (and type) matches.
-        *   Return null if a constructor does not have a parameter which has been supplied.
+        /*  Remove supplied params from each constructor in the list if the name matches.
+        *   We checked that the parameter name and type matches in the subtree, so we can just remove
+        *   the params by name here, which is much easier.
+        *   After we are done, flatten these params to one-level deep. Duplicates are tolerated as they came from
+        *   different levels.
         */
-        val constructorsMinusSuppliedParams: List<List<String>?> =
-                tempNameForConstructorParameterName.map { cParam ->
-                    removeAll(cParam, suppliedParamList.map { it.first })
-                }
+        val constructorsMinusSuppliedParams: List<List<Triple<String, KType, Boolean>>> =
+                fittingConstructors
+                        .map { removeSubtree(suppliedParamList, it.first) }
+                        .map { removeBlanks(it) }
+                        .map { flattenConstructor(it) }
 
-        /*  Associate the actual constructor with the still-unsupplied params
-        *   and remove any null (invalid) constructors.
-        */
-        val validConstructorsWithoutDefaults = constructorsMinusSuppliedParams
-                .zip(constructorList)
-                .mapNotNull { if (it.first == null) null else it }
-                .map { Pair(it.first!!, it.second) }
-
-        /*  Create boolean indicating whether remaining params are optional.
-        *   Associate with the constructor and filter the valid ones by the boolean.
-        *   Return just the valid constructors.
-        */
-        val validConstructors = validConstructorsWithoutDefaults
-                .map { checkTheseParamsOptional(it) }
-                .zip(validConstructorsWithoutDefaults)
-                .map { Pair(it.first, it.second.second) }
+        /*  Check that any remainder fields are either nullable OR have a default.
+         *  Return just the valid constructors.
+         */
+        val validConstructors = constructorsMinusSuppliedParams
+                .map { checkAllFieldsOptional(it) }
+                .zip(fittingConstructors.map { it.second })
                 .filter { it.first }
-                .map { it.second }
 
         // We may now have more than one completely valid constructor, just choose the first one as a tiebreaker.
-        return validConstructors.first()
+        return validConstructors.first().second
     }
 
     // Returns true if all needles exist in haystack.
     // This means true if supplied params match the constructor params but not necessarily all there.
-    private fun includesSubtree(needles: List<Pair<String, Any>>, haystack: List<Pair<String, Any>>): Boolean =
-            needles.map { needl ->
-                val matcher = haystack.find { hay -> hay.first == needl.first }
-                if (matcher == null) {
-                    false
-                } else if (needl.second is KType && matcher.second is KType) {
-                    (needl.second as KType).jvmErasure.primaryConstructor == (matcher.second as KType).jvmErasure.primaryConstructor
-                } else if (needl.second is List<*> && matcher.second is List<*>) {
-                    includesSubtree(needl.second as List<Pair<String, Any>>, matcher.second as List<Pair<String, Any>>)
-                } else {
-                    false
-                }
-
-            }.fold(true) { a, b -> a && b }
-
-    private fun getParameterListWithType(constructor: KFunction<T>): List<Pair<String, KType>> =
-            constructor.parameters.map { Pair(it.name!!, it.type) }
-
-    private fun createParameterArgTree(parameter: Pair<String, KType>): Pair<String, Any> = when (parameter.second.classifier) {
-        // TODO: for this to be really robust we would need to check every single constructor, not just the first one.
-        Int::class -> parameter
-        String::class -> parameter
-        Boolean::class -> parameter
-        Float::class -> parameter
-        else -> Pair(
-                parameter.first,
-                parameter.second.jvmErasure.constructors.first().parameters
-                        .map {
-                            createParameterArgTree(Pair(it.name!!, it.type))
-                        }
-        )
+    private fun includesSubtree(needles: List<Pair<String, Any>>, haystack: List<Triple<String, Any, Boolean>>): Boolean {
+        val a = needles.map { needl ->
+            val matcher = haystack.find { hay -> hay.first == needl.first }
+            if (matcher == null) {
+                false
+            } else if (needl.second is KType && matcher.second is KType) {
+                (needl.second as KType).jvmErasure.primaryConstructor == (matcher.second as KType).jvmErasure.primaryConstructor
+            } else if (needl.second is List<*> && matcher.second is List<*>) {
+                includesSubtree(needl.second as List<Pair<String, Any>>, matcher.second as List<Triple<String, Any, Boolean>>)
+            } else {
+                false
+            }
+        }
+        return a.fold(true) { a, b -> a && b }
     }
 
-    private fun constructorArgTree(constructorArgs: List<Pair<String, KType>>): List<Pair<String, Any>> =
-            constructorArgs.map { createParameterArgTree(it) }
+    private fun getParameterListWithTypeAndOptionality(constructor: KFunction<T>): List<Triple<String, KType, Boolean>> =
+            constructor.parameters.map { Triple(it.name!!, it.type, it.isOptional) }
+
+    private fun createParameterArgTree(params: List<KParameter>): List<Triple<String, Any, Boolean>> {
+        val a = params.map { p ->
+            when (p.type.classifier) {
+                // TODO: for this to be really robust we would need to check every single constructor, not just the first one.
+                Int::class -> Triple(p.name!!, p.type, p.isOptional)
+                String::class -> Triple(p.name!!, p.type, p.isOptional)
+                Boolean::class -> Triple(p.name!!, p.type, p.isOptional)
+                Float::class -> Triple(p.name!!, p.type, p.isOptional)
+                else -> Triple(
+                        p.name!!,
+                        createParameterArgTree(p.type.jvmErasure.constructors.first().parameters),
+                        p.isOptional)
+            }
+        }
+        return a
+    }
+
+    private fun constructorArgTree(constructor: KFunction<T>): List<Triple<String, Any, Boolean>> =
+            createParameterArgTree(constructor.parameters)
 
     private fun paramNamesWithTypes(params: List<Triple<String, KType, Boolean>>): List<Pair<String, KType>> =
             params.map { Pair(it.first, it.second) }
 
-    private fun removeIfExists(haystack: List<String>, needle: String): List<String>? {
-        return if (haystack.find { it == needle } != null) {
-            haystack.filterNot { it == needle }
+    private fun removeIfExists(haystack: List<Pair<String, Any>>, needle: String): List<Pair<String, Any>>? {
+        return if (haystack.find { it.first == needle } != null) {
+            haystack.filterNot { it.first == needle }
         } else {
             null
         }
     }
 
     // Returns null if a needle does not exist.
-    private fun removeAll(haystack: List<String>?, needles: List<String>): List<String>? {
+    private fun removeAll(haystack: List<Pair<String, Any>>?, needles: List<Pair<String, Any>>): List<Pair<String, Any>>? {
         if (haystack == null) {
             return null
         }
         if (needles.isEmpty()) {
             return haystack
         }
-        val newHaystack = removeIfExists(haystack, needles.first())
+        val newHaystack = removeIfExists(haystack, needles.first().first)
         return removeAll(newHaystack, needles.slice(1..needles.lastIndex))
+    }
+
+    private fun removeSubtree(needles: List<Pair<String, Any>>, haystack: List<Triple<String, Any, Boolean>>): List<Triple<String, Any?, Boolean>> {
+        val a = haystack.map { hay ->
+            val needl = needles.find { it.first == hay.first }
+            if (needl == null) {
+                hay
+            } else {
+                when (needl!!.second) {
+                    is KType -> null
+                    is List<*> -> Triple(
+                            needl.first,
+                            removeSubtree(needl.second as List<Pair<String, Any>>, hay.second as List<Triple<String, Any, Boolean>>),
+                            hay.third
+                    )
+                    else -> throw Error("WHAAAAAT?")
+                }
+            }
+        }
+        return a.filterNotNull()
+    }
+
+    private fun removeBlanks(haystack: List<Triple<String, Any?, Boolean>>): List<Triple<String, Any?, Boolean>> {
+        val a = haystack.map { hay ->
+            if (hay.second is List<*> && (hay.second as List<*>).isEmpty()) {
+                null
+            } else {
+                when (hay.second) {
+                    is KType -> hay
+                    is List<*> -> removeBlanks(hay.second as List<Triple<String, Any?, Boolean>>)
+                    else -> throw Error("NOOOO")
+                }
+            }
+        }
+        return a.filterNotNull() as List<Triple<String, Any?, Boolean>>
+    }
+
+    private fun checkAllFieldsOptional(params: List<Triple<String, KType, Boolean>>): Boolean {
+        val a = params.map {
+            it.second.isMarkedNullable || it.third
+        }
+        return a.fold(true) { c, b -> c && b }
+    }
+
+    private fun flattenConstructor(params: List<Any>): List<Triple<String, KType, Boolean>> {
+        val a = params.map { p ->
+            when (p) {
+                is Triple<*, *, *> -> p
+                is List<*> -> flattenConstructor(p as List<Any>)
+                else -> throw Error("")
+            }
+        }
+        val b = rollupOneLevel(a)
+        return b as List<Triple<String, KType, Boolean>>
+    }
+
+    private fun rollupOneLevel(list: List<Any>): List<Triple<String, KType, Boolean>> {
+        val a = list.fold(listOf<Triple<String, KType, Boolean>>()) { acc, p ->
+            when (p) {
+                is Triple<*, *, *> -> acc + p as Triple<String, KType, Boolean>
+                is List<*> -> acc.plus(p as List<Triple<String, KType, Boolean>>)
+                else -> throw Error("")
+            }
+        }
+        return a
     }
 
     private fun checkTheseParamsOptional(paramPair: Pair<List<String>, KFunction<T>>): Boolean {
